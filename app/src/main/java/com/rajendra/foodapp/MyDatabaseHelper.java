e51e0eb4334ff38a65e00c322f3ffdeed8bf9645
niemet0502 <vincentmarius8@gmail.com>
2021-03-19 22:05:45 +0100

    main :: create Helper for Batabase

---
+++ app/src/main/java/com/rajendra/foodapp/MyDatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.rajendra.foodapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private Context context;
+    public static final String DATABASE_NAME = "Food.db";
+    public static final int  DATABASE_VERSION = 1;
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
