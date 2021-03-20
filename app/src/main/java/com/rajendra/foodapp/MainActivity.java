16d94030900d29327c143060fd219944bfc2fa53
niemet0502 <vincentmarius8@gmail.com>
2021-03-20 02:27:31 +0100

    main :: displayed login and signup layout

--- app/src/main/java/com/rajendra/foodapp/MainActivity.java
+++ app/src/main/java/com/rajendra/foodapp/MainActivity.java
@@ -5,21 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView myButton;
+    Button myButton;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myButton = (ImageView) findViewById(R.id.home_button);
+        myButton = (Button) findViewById(R.id.sign_button);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomeClientActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
