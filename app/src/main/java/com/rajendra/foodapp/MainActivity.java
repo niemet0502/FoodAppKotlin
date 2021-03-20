9001454f3e690eb2dd626f17a2b07ec976e9d3a6
niemet0502 <vincentmarius8@gmail.com>
2021-03-20 01:33:35 +0100

    main :: Main Activity update

--- app/src/main/java/com/rajendra/foodapp/MainActivity.java
+++ app/src/main/java/com/rajendra/foodapp/MainActivity.java
@@ -1,111 +1,27 @@
 package com.rajendra.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.rajendra.foodapp.adapter.AllMenuAdapter;
-import com.rajendra.foodapp.adapter.PopularAdapter;
-import com.rajendra.foodapp.adapter.RecommendedAdapter;
-import com.rajendra.foodapp.model.Allmenu;
-import com.rajendra.foodapp.model.FoodData;
-import com.rajendra.foodapp.model.Popular;
-import com.rajendra.foodapp.model.Recommended;
-import com.rajendra.foodapp.retrofit.ApiInterface;
-import com.rajendra.foodapp.retrofit.RetrofitClient;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    ApiInterface apiInterface;
-
-    RecyclerView popularRecyclerView, recommendedRecyclerView, allMenuRecyclerView;
-
-    PopularAdapter popularAdapter;
-    RecommendedAdapter recommendedAdapter;
-    AllMenuAdapter allMenuAdapter;
-
     ImageView myButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
-
-        Call<List<FoodData>> call = apiInterface.getAllData();
-        call.enqueue(new Callback<List<FoodData>>() {
-            @Override
-            public void onResponse(Call<List<FoodData>> call, Response<List<FoodData>> response) {
-
-                List<FoodData> foodDataList = response.body();
-
-
-                getPopularData(foodDataList.get(0).getPopular());
-
-                getRecommendedData(foodDataList.get(0).getRecommended());
-
-                getAllMenu(foodDataList.get(0).getAllmenu());
-
-            }
-
-            @Override
-            public void onFailure(Call<List<FoodData>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Server is not responding.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        myButton = (ImageView) findViewById(R.id.cart_image);
+        myButton = (ImageView) findViewById(R.id.home_button);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CartActivity.class);
+                Intent intent = new Intent(MainActivity.this, HomeClientActivity.class);
                 startActivity(intent);
             }
         });
-
-    }
-
-    private void  getPopularData(List<Popular> popularList){
-
-        popularRecyclerView = findViewById(R.id.popular_recycler);
-        popularAdapter = new PopularAdapter(this, popularList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        popularRecyclerView.setLayoutManager(layoutManager);
-        popularRecyclerView.setAdapter(popularAdapter);
-
-    }
-
-    private void  getRecommendedData(List<Recommended> recommendedList){
-
-        recommendedRecyclerView = findViewById(R.id.recommended_recycler);
-        recommendedAdapter = new RecommendedAdapter(this, recommendedList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recommendedRecyclerView.setLayoutManager(layoutManager);
-        recommendedRecyclerView.setAdapter(recommendedAdapter);
-
-    }
-
-    private void  getAllMenu(List<Allmenu> allmenuList){
-
-        allMenuRecyclerView = findViewById(R.id.all_menu_recycler);
-        allMenuAdapter = new AllMenuAdapter(this, allmenuList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        allMenuRecyclerView.setLayoutManager(layoutManager);
-        allMenuRecyclerView.setAdapter(allMenuAdapter);
-        allMenuAdapter.notifyDataSetChanged();
-
     }
 }
