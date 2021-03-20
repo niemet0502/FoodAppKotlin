15ca48dd4393d55927b7ea118ed0f8b67eaa2ccb
niemet0502 <vincentmarius8@gmail.com>
2021-03-20 02:52:02 +0100

    main :: displayed forms

--- app/src/main/java/com/rajendra/foodapp/LoginActivity.java
+++ app/src/main/java/com/rajendra/foodapp/LoginActivity.java
@@ -2,13 +2,27 @@ package com.rajendra.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        button = (Button) findViewById(R.id.log_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeClientActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
