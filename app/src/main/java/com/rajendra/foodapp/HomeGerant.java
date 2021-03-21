d40e10b7cb7f724dc43ad097ed614d05f60f1139
niemet0502 <vincentmarius8@gmail.com>
2021-03-21 06:51:36 +0100

    main :: created HomeGerant class

---
+++ app/src/main/java/com/rajendra/foodapp/HomeGerant.java
@@ -0,0 +1,14 @@
+package com.rajendra.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeGerant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_gerant);
+    }
+}
