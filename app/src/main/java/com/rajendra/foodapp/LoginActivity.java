cdfd62065a09e163789bcba1b2cbf8ae58cad3b4
niemet0502 <vincentmarius8@gmail.com>
2021-03-21 06:51:52 +0100

    main :: added function to log in database

--- app/src/main/java/com/rajendra/foodapp/LoginActivity.java
+++ app/src/main/java/com/rajendra/foodapp/LoginActivity.java
@@ -6,23 +6,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button button;
+    private  SharedPreferenceConfig sharedPreferenceConfig;
+    EditText username;
+    EditText password;
+    private DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.signupemail);
+        password = (EditText) findViewById(R.id.signupusername);
+        sharedPreferenceConfig = new SharedPreferenceConfig(getApplicationContext());
+
+        myDb = new DatabaseHelper(this);
 
 
         button = (Button) findViewById(R.id.log_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, HomeClientActivity.class);
-                startActivity(intent);
+                Login();
             }
         });
     }
+
+    private void Login() {
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        boolean val = myDb.checkUser(user, pass);
+        if(user.equals(getResources().getString(R.string.username)) && pass.equals(getResources().getString(R.string.password))){
+            Intent intent = new Intent(LoginActivity.this, HomeGerant.class);
+            startActivity(intent);
+            sharedPreferenceConfig.login_status(true);
+            finish();
+        }else {
+
+            if(val){
+                Intent intent = new Intent(LoginActivity.this, HomeClientActivity.class);
+                startActivity(intent);
+                sharedPreferenceConfig.login_status(true);
+                finish();
+            }else{
+                Toast.makeText(this, "Login ou mot de passe invalide", Toast.LENGTH_SHORT).show();
+                username.setText("");
+                password.setText("");
+            }
+
+        }
+    }
+
 }
