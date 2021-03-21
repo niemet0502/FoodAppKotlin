0fbe8d9927ade00dddbe84f9a016c17ac3ee864e
niemet0502 <vincentmarius8@gmail.com>
2021-03-21 06:52:26 +0100

    main :: SignUpActivity class

--- app/src/main/java/com/rajendra/foodapp/SignUpActivity.java
+++ app/src/main/java/com/rajendra/foodapp/SignUpActivity.java
@@ -3,12 +3,41 @@ package com.rajendra.foodapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private DatabaseHelper myDB;
+    EditText nom;
+    EditText mail;
+    EditText password;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mail = findViewById(R.id.signupemail);
+        nom = findViewById(R.id.signupusername);
+        password = findViewById(R.id.siguppassword);
+
+        myDB = new DatabaseHelper(this);
+
+
+        button = findViewById(R.id.sign_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                boolean var = myDB.registerUser(nom.getText().toString() , mail.getText().toString() , password.getText().toString());
+                if(var){
+                    Toast.makeText(SignUpActivity.this, "Utilisateur enregistree!!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(SignUpActivity.this, "Erreur !!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
