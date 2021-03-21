30732fc9cec18bd0c92bc75457480946de9aa3fe
niemet0502 <vincentmarius8@gmail.com>
2021-03-21 06:50:53 +0100

    main :: DatabaseHelper creation

--- app/src/main/java/com/rajendra/foodapp/DatabaseHelper.java
+++ app/src/main/java/com/rajendra/foodapp/DatabaseHelper.java
@@ -1,4 +1,94 @@
 package com.rajendra.foodapp;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // Logcat tag
+    private static final String LOG = "DatabaseHelper";
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "FoodApp";
+
+
+    // Table Names
+    private static final String TABLE_TODO = "users";
+
+    // Common column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_CREATED_AT = "created_at";
+
+    // NOTES Table - column nmaes
+    private static final String KEY_NOM = "nom";
+    private static final String KEY_MAIL = "mail";
+    private static final String KEY_PASSWORD = "password";
+
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "nom";
+    private static final String COL_3 = "mail";
+    private static final String COL_4 = "password";
+
+
+    // Table Create Statements
+    // Todo table create statement
+    private static final String CREATE_TABLE_USER = "CREATE TABLE "
+            + TABLE_TODO + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NOM
+            + " TEXT," + KEY_MAIL + " TEXT," + KEY_PASSWORD + " TEXT," + KEY_CREATED_AT
+            + " DATETIME" + ")";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // creating required tables
+        db.execSQL(CREATE_TABLE_USER);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+    }
+
+    public boolean registerUser(String username , String email , String password){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nom" , username);
+        values.put("main" , email);
+        values.put("password" , password);
+
+        long result = db.insert(TABLE_TODO , null , values);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean checkUser(String username , String password){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String [] columns = { COL_1 };
+        String selection = COL_2 + "=?" + " and " + COL_4 + "=?";
+        String [] selectionargs = { username , password};
+        Cursor cursor = db.query(TABLE_TODO , columns , selection ,selectionargs , null , null , null);
+        int count = cursor.getCount();
+        db.close();
+        cursor.close();
+        if (count > 0)
+            return true;
+        else
+            return false;
+
+    }
 }
